77d5fe4e54c422e6fe62c53d7d4c85d7b78dd7cd
Nurio Fernandez <personal@nurio.me>
2022-07-10 19:04:01 +0200

    Implemented an initial usage

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"GTASAGO/memoryReader"
 	"fmt"
+	. "github.com/0xrawsec/golang-win32/win32"
 )
 
 func main() {
-	fmt.Println("WIP")
+	process := memoryReader.FindProcessByName("gta_sa.exe")
+	pid := DWORD(process.ProcessID)
+
+	// get ped 0xB6F5F0 *dword
+	// get status from ped + 0x46C *uint8
+	pedDWORD := memoryReader.ReadDWORD(pid, 0xB6F5F0)
+	statusByte := memoryReader.ReadByte(pid, pedDWORD+0x46C)
+
+	//hexPointer := hex.EncodeToString(baseAddress)
+	fmt.Println("Ped status: (0=air/water, 1=in vehicle, 2=entering interior, 3=in floor) -> ", statusByte)
 }
