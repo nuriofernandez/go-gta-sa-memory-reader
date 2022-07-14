60afea232cae6f6bf2170e7f5319c0b1d43aeb09
Nurio Fernandez <personal@nurio.me>
2022-07-14 03:26:43 +0200

    Implemented gtaMemoryAccessor to the main file

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	. "github.com/0xrawsec/golang-win32/win32"
+	"github.com/xXNurioXx/go-gta-sa-memory-reader/gtaMemoryAccessor"
 	"github.com/xXNurioXx/go-gta-sa-memory-reader/memoryReader"
 )
 
@@ -10,13 +11,10 @@ func main() {
 	process := memoryReader.FindProcessByName("gta_sa.exe")
 	pid := DWORD(process.ProcessID)
 
-	// get ped 0xB6F5F0 *dword
-	// get status from ped + 0x46C *uint8
-	pedDWORD := memoryReader.ReadDWORD(pid, 0xB6F5F0)
-	statusByte := memoryReader.ReadByte(pid, pedDWORD+0x46C)
-	money := memoryReader.ReadInt(pid, 0xB7CE50)
+	gtaApi := gtaMemoryAccessor.GtaApi{PID: pid}
+	statusByte := gtaApi.GetPedStatus()
+	money := gtaApi.GetMoney()
 
-	//hexPointer := hex.EncodeToString(baseAddress)
 	fmt.Println("Ped status: (0=air/water, 1=in vehicle, 2=entering interior, 3=in floor) ->", statusByte)
 	fmt.Println("User money is ->", money)
 }
