5dce9e61a2d48014cb3e992bb0309b9348f1e254
Nurio Fernandez <personal@nurio.me>
2022-07-14 02:48:34 +0200

    Added money memory reader

--- main.go
+++ main.go
@@ -14,7 +14,9 @@ func main() {
 	// get status from ped + 0x46C *uint8
 	pedDWORD := memoryReader.ReadDWORD(pid, 0xB6F5F0)
 	statusByte := memoryReader.ReadByte(pid, pedDWORD+0x46C)
+	money := memoryReader.ReadInt(pid, 0xB7CE50)
 
 	//hexPointer := hex.EncodeToString(baseAddress)
-	fmt.Println("Ped status: (0=air/water, 1=in vehicle, 2=entering interior, 3=in floor) -> ", statusByte)
+	fmt.Println("Ped status: (0=air/water, 1=in vehicle, 2=entering interior, 3=in floor) ->", statusByte)
+	fmt.Println("User money is ->", money)
 }
